6e3ed5d043b1143f064f0b40c67a5a179fe90515
Safa <safa.howaid@gmail.com>
2022-05-02 13:12:54 -0400

    Fix api calls

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inventory-tracker-web.herokuapp.com/api",
+  baseURL: "http://localhost:8000/api",
 });
 
 export const insertProduct = (payload) => api.post(`/product`, payload);
